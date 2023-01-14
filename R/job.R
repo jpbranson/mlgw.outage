2261330f03f18ca1dd5080110757674f5bb42557
jpbranson <jpbranson@jpbransons-iMac.local>
2023-01-13 19:43:34 -0600

    didn't assign object to be written

--- R/job.R
+++ R/job.R
@@ -108,7 +108,7 @@ new_polygon <- page_text %>%
 total_parsed_polygons <- readRDS("total_parsed_polygons.rds") %>%
   bind_rows(new_polygon)
 
-saveRDS("total_parsed_polygons.rds")
+saveRDS(total_parsed_polygons, "total_parsed_polygons.rds")
 
 mygrid <- data.frame(
   row = c(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11),
